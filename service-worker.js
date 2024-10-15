f478786c108511cfe1bbdbd43a7a55c0a06f0260
CastelWebs <152329148+CastelWebs@users.noreply.github.com>
2024-10-15 17:50:08 -0600

    Update and rename serviceWorker.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open('ruleta-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.js',
+        '/index.css',
+        '/jQueryRotate',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
